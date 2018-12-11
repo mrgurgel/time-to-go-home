f344e3f0cbc3e1149307ac4c2502808cd848ceee
Márcio Gurgel <marcio.rga@gmail.com>
2018-12-11 15:34:12 -0200

    Improving the result output

--- lifter/calculator.py
+++ lifter/calculator.py
@@ -14,6 +14,6 @@ hours_sum = datetime.strptime("00:00", "%H:%M")
 for iteracao in range(0, len(hours), 2):
     hours_sum = hours_sum + (hours[iteracao + 1] - hours[iteracao])
 
-print("I worked {:0>2d}:{:0>2d}".format(hours_sum.hour, hours_sum.minute))
+print("I worked {:0>2d}:{:0>2d} hours".format(hours_sum.hour, hours_sum.minute))
 
 
